78b26d402124de7bedce9eca6217d533c3e5a9d6
Santiago Alvarez Ochoa <112513439+santiagoprogramador99@users.noreply.github.com>
2024-07-09 16:07:02 -0500

    Modificacion script Player

--- Assets/Scripts/Playermovement.cs
+++ Assets/Scripts/Playermovement.cs
@@ -2,26 +2,20 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float moveSpeed = 5f;
-    public float rotationSpeed = 700f;
+    public float rotationSpeed = 100f;
 
     void Update()
     {
-        // Obtener las entradas del usuario
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
-
-        // Crear un vector de movimiento basado en las entradas
-        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical).normalized;
-
-        // Aplicar el movimiento al transform del jugador
-        transform.Translate(movement * moveSpeed * Time.deltaTime, Space.World);
-
-        // Si hay movimiento, rota el jugador hacia la dirección del movimiento
-        if (movement != Vector3.zero)
+        // Rotar hacia la derecha
+        if (Input.GetKey(KeyCode.RightArrow))
         {
-            Quaternion toRotation = Quaternion.LookRotation(movement, Vector3.up);
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
+            transform.Rotate(Vector3.forward, -rotationSpeed * Time.deltaTime);
+        }
+
+        // Rotar hacia la izquierda
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(Vector3.forward, rotationSpeed * Time.deltaTime);
         }
     }
 }
